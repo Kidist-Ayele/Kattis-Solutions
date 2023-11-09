864fd7397c27ecf520935d8e0763cf11947811a2
Kidist_Ayele <kidistayele37@gmail.com>
2023-11-09 14:31:34 -0800

    my first problem

---
+++ Amerískur vinnustaður.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int main(){
+    int n;
+    cin>>n;
+    double ans = n*0.09144;
+    cout<<setprecision(17)<<ans<<endl;
+    return 0;
+
+
+}
+//set precision
