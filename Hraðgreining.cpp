f9f5add5a1c1facb51e8a92aa94395857747dcfb
Kidist_Ayele <kidistayele37@gmail.com>
2023-11-15 09:03:24 -0800

    new add

---
+++ Hraðgreining.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h> 
+
+using namespace std; 
+
+int main() { 
+    string sentence;
+    cin>>sentence ;
+     string segment = "COV"; 
+     int found = sentence.find(segment); 
+     if (found != string::npos) { 
+        cout << "Veikur!" << endl; 
+    } else { 
+        cout << "Ekki veikur!" << endl; 
+        } 
+    return 0; 
+}
