8ad12d9bb232c2d6e4620b24be2c5d1abd92b3fc
Kidist_Ayele <kidistayele37@gmail.com>
2023-11-15 10:29:21 -0800

    Á leið í bíó

---
+++ 004-cinema.cpp
@@ -0,0 +1,15 @@
+
+# include <bits/stdc++.h> 
+
+using namespace std; 
+
+int main() { 
+    int a,b,c,x,result;
+    cin>>a>>b>>c;
+    
+    x=a+b;
+    result = c-x;
+
+    cout<<result<<endl;
+    return 0; 
+}
