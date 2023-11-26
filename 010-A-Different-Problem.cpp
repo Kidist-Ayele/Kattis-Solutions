5a86279882936b778b86359ca28994052faf876f
Kidist_Ayele <kidistayele37@gmail.com>
2023-11-26 00:20:03 -0800

    difference

---
+++ 010-A-Different-Problem.cpp
@@ -0,0 +1,12 @@
+# include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long long a,b,x,ans;
+   while( cin>>a>>b){
+    ans = a-b;
+     x = abs(ans);
+    cout<<x <<endl;
+   }
+    return 0;
+
+}
