58290b3d3fde2b741ed35c82c4f5a123e79ca584
Kidist_Ayele <kidistayele37@gmail.com>
2024-03-02 11:44:43 -0800

    new solution

---
+++ 015-GCD.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b ,k;
+    cin>>a>>b;
+    int result = gcd(a, b);
+    cout<<result;
+    return 0;
+}
+// built in function gcd to calculate greatest common divisor of two numbers inport <numerics>
